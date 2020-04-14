defa9bad793decbaa2ac502d917c650b350e2c7d
noveni <nico.novello@gmail.com>
2020-04-14 04:39:50 +0200

    add basic js

--- src/scripts/theme/index.js
+++ src/scripts/theme/index.js
@@ -3,3 +3,17 @@ import 'img/logo.svg';
 
 import { ecrannoirDomReady } from 'scripts/utils/dom';
 import Router from 'scripts/utils/Router';
+
+
+import common from './routes/common';
+import home from './routes/home'
+
+
+/** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+  // Home page
+  home,
+});
+ecrannoirDomReady(() => routes.loadEvents())
---
+++ src/scripts/theme/routes/home.js
@@ -0,0 +1,9 @@
+
+// eslint-disable-next-line no-unused-vars
+export default {
+  init() {
+  },
+  finalize() {
+    // JavaScript to be fired on the home page, after the init JS
+  },
+};
